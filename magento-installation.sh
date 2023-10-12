9fe2e93ae65cf76dfacfdd4261f152afddb82c62
Matthijs <github@muyncky.eu>
2023-10-12 16:39:55 +0200

    feat: Installing Magento

--- magento-installation.sh
+++ magento-installation.sh
@@ -7,34 +7,38 @@ echo "Waiting for the $IMAGE_NAME container to be ready..."
 
 while true; do
     # Get the container ID based on the image name
-    CONTAINER_ID=$(docker ps | grep magento2 | awk '{print $1}')
+    DB_CONTAINER=$(docker ps | grep '\-db' | awk '{print $1}')
     
-    if [[ -z "$CONTAINER_ID" ]]; then
+    if [[ -z "$DB_CONTAINER" ]]; then
         echo "Container with image $IMAGE_NAME is not yet started. Waiting..."
         sleep 2
         continue
     fi
 
     # Check the container's status
-    CONTAINER_STATUS=$(docker inspect --format '{{.State.Status}}' "$CONTAINER_ID")
+    CONTAINER_STATUS=$(docker inspect --format '{{.State.Status}}' "$DB_CONTAINER")
 
-    if [[ "$CONTAINER_STATUS" == "running" ]]; then
+    if [[ $CONTAINER_STATUS -eq "running" ]]; then
         echo "Container with image $IMAGE_NAME is running."
 
         # Wait for MySQL to be ready
-        echo "Checking database readiness..."
-        for i in {1..30}; do
-            if docker exec "$CONTAINER_ID" mysqladmin ping -h"db" -u"magento" -p"magento" &>/dev/null; then
+
+        # echo "Checking database readiness..."
+        for i in {1..150}; do
+            # docker exec $DB_CONTAINER sh -c "mysqladmin ping -h'db' -u'magento' -p'magento'"
+
+            if docker exec $DB_CONTAINER sh -c "mysqladmin ping -h'db' -u'magento' -p'magento'" &>/dev/null; then
                 echo "Database is ready."
                 break
             fi
-            echo "Waiting for database to initialize..."
-            sleep 5
+            echo "Waiting for database to initialize.. $i"
+            sleep 1
         done
 
+        MAGENTO_CONTAINER=$(docker ps | grep '\-magento' | awk '{print $1}')
 
         # Execute your desired command here
-        docker exec $CONTAINER_ID sh -c "php bin/magento setup:install \
+        docker exec $MAGENTO_CONTAINER sh -c "php bin/magento setup:install \
             --admin-firstname=John \
             --admin-lastname=Doe \
             --admin-email=johndoe@example.com \
@@ -57,10 +61,12 @@ while true; do
             --use-sample-data \
             && php bin/magento cache:flush \
             && chown -R www-data:www-data ."
-        
-        break
+
+        docker exec $MAGENTO_CONTAINER sh -c "php bin/magento config:set payment/checkmo/active 1"
+
+        exit 0
     else
-        echo "Waiting for container $CONTAINER_ID to start..."
+        echo "Waiting for container $DB_CONTAINER to start..."
         sleep 2
     fi
 done
